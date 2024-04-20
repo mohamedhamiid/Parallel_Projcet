0fdd0f39aec5608ff2c00695922ea2c07b6d7d52
Mohamed Abdel Hamid <111571591+mohamedhamiid@users.noreply.github.com>
2024-04-20 19:22:18 +0200

    Delete ec directory

--- ec/app/url.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-# In this file we add all URLs related to the app (APP URL File)
-# Then include all in main URL
-# Hamid
-urlpatterns = [
-    path("", views.Home), # Home
-]
