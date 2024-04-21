17883e5407625647339e953a93600c8d604ff0c5
Anas Hamed Ahmed Hamed <111208532+Anas2108@users.noreply.github.com>
2024-04-22 00:27:25 +0200

    Add New Pages
    Added Pages :
    - Contact us Page
    - Login Page
    - Registration Page

--- ec/app/url.py
+++ ec/app/url.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_view
+from .forms import LoginForm,MyPasswordResetForm
 
 # In this file we add all URLs related to the app (APP URL File)
 # Then include all in main URL
@@ -13,4 +15,11 @@ urlpatterns = [
     path("category/<slug:val>", views.CategoryView.as_view(),name="category"),
     path("category-title/<val>", views.CategoryTitle.as_view(),name="category-title"),
     path("product-detail/<int:pk>", views.ProductDetail.as_view(),name="product-detail"),
+
+    #Anas
+    #Add login authentication
+    path('registration/', views.CustomerRegistrationView.as_view(), name='customerregistration'), #path of registration
+    path('accounts/login/', auth_view.LoginView.as_view(template_name='app/login.html',authentication_form=LoginForm), name='login'), #path of login
+    path('password-reset/',auth_view.PasswordResetView.as_view(template_name='app/password_reset.html',form_class=MyPasswordResetForm),name='password_reset'),
+
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ec/app/views.py
+++ ec/app/views.py
@@ -2,6 +2,9 @@ from django.db.models import Count
 from django.shortcuts import render 
 from django.views import View
 from . models import Product
+from django.contrib import messages
+#Anas
+from .forms import CustomerRegistrationForm
 # Create your views here.
 
 def home(request):
@@ -29,3 +32,21 @@ class ProductDetail(View):
     def get(self, request, pk):
         product = Product.objects.get(pk=pk)
         return render(request, 'app/productdetail.html', locals())
+    
+# Anas
+# add customer registeration view   
+
+class CustomerRegistrationView(View):
+    def get(self,request):
+        form = CustomerRegistrationForm()
+        return render(request,'app/customerregistration.html',locals())
+    def post(self,request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"User Register Successfully")
+        else:
+            messages.warning(request,"Invalid Input Data")
+        return render(request,'app/customerregistration.html',locals())
+    
+                
